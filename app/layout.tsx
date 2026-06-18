c3cc1662af514c3b6512af141dbc6cacbf8687e1
Hezekiah <mellomarsh686@gmail.com>
2026-06-18 19:56:32 +0800

    spped insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 // @ts-ignore
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics/>
+      <SpeedInsights/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
